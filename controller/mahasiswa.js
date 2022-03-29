a0986793b2972b6fc3aaf4bcebf41798925c9511
Dinda Lutfiyah <dinda.lutfiyah@gmail.com>
2022-03-29 16:41:24 +0700

    melengkapi controller

--- controller/mahasiswa.js
+++ controller/mahasiswa.js
@@ -3,12 +3,11 @@ const controller = {};
 
 controller.getAll = async function(req,res) {
     try {
-        await model.mahasiswa.findAll()
-        .then ((result) => {
-            if (result.length > 0) {
+        let mahasiswa = await model.mahasiswa.findAll()
+            if (mahasiswa.length > 0) {
                 res.status(200).json({
-                    message: "Get Method Mahasiswa",
-                    data: result
+                    message: "Get data semua Mahasiswa",
+                    data: mahasiswa
                 })
             }else{
                 res.status(200).json({
@@ -16,12 +15,92 @@ controller.getAll = async function(req,res) {
                     data:[]
                 })
             }
-        })
     } catch (error) {
         res.status(404).json({
             message: error
         })
     }
 }
+
+controller.getOne = async function(req,res) {
+    try {
+        let mahasiswa = await model.mahasiswa.findAll({
+            where: {
+                nim: req.params.nim
+            }
+        })
+        if (mahasiswa.length > 0) {
+            res.status(200).json({
+                message: 'Mahasiswa Ditemukan',
+                data: mahasiswa
+            })
+        }else {
+            res.status(200).json({
+                message: 'Tidak ada Data',
+                data: []
+            })
+        }
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        })
+    }
+}
+
+controller.post = async function(req,res) {
+    try {
+        let mahasiswa = await model.mahasiswa.create({
+            nim: req.body.nim,
+            nama: req.body.nama,
+            jurusan: req.body.jurusan
+        })
+        res.status(201).json({
+            message: "Berhasill tambah data mahasiswa",
+            data: mahasiswa
+        })
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        })
+    }
+}
+
+controller.put = async function(req,res) {
+    try {
+        let mahasiswa = await model.mahasiswa.update({
+            nama: req.body.nama,
+            jurusan: req.body.jurusan
+        },{
+            where: {
+                nim: req.params.nim
+            }
+        })
+        res.status(200).json({
+            message: "Berhasil update data"
+        })
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        })
+    }
+}
+
+controller.delete = async function(req,res) {
+    try {
+        let mahasiswa = await model.mahasiswa.destroy({
+            where: {
+                nim: req.params.nim
+            }
+        })
+        res.status(200).json({
+            message: "Berhasil ubah data"
+        })
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        })
+    }
+}
+
 
 module.exports = controller;
--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -5,80 +5,9 @@ const controller = require('../controller/index');
 
 //mengambil data mahasiswa
 router.get('/', controller.mahasiswa.getAll);
-
-//memasukan data mahasiswa
-router.post('/', (req,res,next) => {
-    const nim = req.body.nim;
-    const nama = req.body.nama;
-    const jurusan = req.body.jurusan;
-    var sql = "INSERT INTO mahasiswa (nim, nama, jurusan) VALUES ('"+nim+"', '"+nama+"', '"+jurusan+"')";
-    db.query(sql, (err, result) => {
-        if (err) throw err;
-        res.status(200).json({
-            message: 'Berhasil Tambah Data Mahasiswa'
-        })
-    }) 
-})
-
-//mencari mahasiswa berdasarkan nim
-//re.param adalah cara mengirim nilai lewat url
-//re.query adlaah cara mengirim nilai tidak di url tpi lewat postman yang params key value
-router.get('/search', (req,res,next) => {
-    const nim = req.query.nim;
-    var sql = "SELECT * FROM mahasiswa WHERE nim="+nim;
-    db.query(sql, (err, result) => {
-        if (err) throw err;
-        if (result.length > 0) {
-            res.status(200).json({
-                message: 'mahasiswa ditemukan',
-                data: result
-            })
-        } else {
-            res.status(200).json({
-                message: 'mahasiswa tidak ditemukan',
-                data: result
-            })
-        }
-    }) 
-})
-
-router.get('/:nim', (req,res,next) => {
-    const nim = req.params.nim;
-    var sql = "SELECT * FROM mahasiswa WHERE nim="+nim;
-    db.query(sql, (err, result) => {
-        if (err) throw err;
-        res.status(200).json({
-            message: 'mahasiswa ditemukan',
-            data: result
-        })
-    }) 
-})
-
-//update data mahasiswa
-//re.body itu untuk mengirim data atau update data
-router.put('/', (req,res,next) => {
-    const nim = req.body.nim;
-    const nama = req.body.nama;
-    const jurusan = req.body.jurusan;
-    var sql = "UPDATE mahasiswa SET nama = '"+nama+"', jurusan = '"+jurusan+"' WHERE nim = '"+nim+"'";
-    db.query(sql, (err, result) => {
-        if (err) throw err;
-        res.status(200).json({
-            message: 'Berhasil Ubah Data Mahasiswa'
-        })
-    }) 
-})
-
-//delete data mahasiswa
-router.delete('/:nim', (req,res,next) => {
-    const nim = req.params.nim;
-    var sql = "DELETE FROM mahasiswa WHERE nim = '"+nim+"'";
-    db.query(sql, (err, result) => {
-        if (err) throw err;
-        res.status(200).json({
-            message: 'Berhasil Delete Data Mahasiswa'
-        })
-    }) 
-})
+router.get('/:nim', controller.mahasiswa.getOne);
+router.post('/', controller.mahasiswa.post);
+router.put('/:nim', controller.mahasiswa.put);
+router.delete('/:nim', controller.mahasiswa.delete);
 
 module.exports = router;
