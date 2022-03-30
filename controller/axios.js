7fb5dc40fb41dcfc287e942971fa909202b9ae96
Dinda Lutfiyah <dinda.lutfiyah@gmail.com>
2022-03-30 14:23:11 +0700

    Cara ke 3 get data API public

--- controller/axios.js
+++ controller/axios.js
@@ -2,22 +2,24 @@ const axios = require('axios');
 const controller = {}
 
 controller.getAll = async function (req,res){
-    axios({
-        method: 'get',
-        url: 'http://jsonplaceholder.typicode.com/posts'
-    })
-    //axios.get('http://jsonplaceholder.typicode.com/posts')
-    .then(function(response){
-        res.status(200).json({
-            message: 'Data dari public API',
-            data: response.data
-        })
-    })
-    .catch(function(error){
+    try {
+        const response = await axios.get('http://jsonplaceholder.typicode.com/posts');
+        if (response.data.length > 0) {
+            res.status(200).json({
+                message: "Data dari public API",
+                data: response.data
+            })
+        }else{
+            res.status(200).json({
+                message: "Tidak ada data",
+                data:[]
+            })
+        }
+    } catch (error) {
         res.status(404).json({
-            message: error.message
+            message: error
         })
-    })
+    }
 }
 
 module.exports = controller;
