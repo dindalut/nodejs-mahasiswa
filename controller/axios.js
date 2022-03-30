4f7e29c78c3ad5e1edf5c78cdf04f9578b9757c3
Dinda Lutfiyah <dinda.lutfiyah@gmail.com>
2022-03-30 14:49:41 +0700

    Asynchronous http request dengan axios di node js

--- controller/axios.js
+++ controller/axios.js
@@ -22,4 +22,67 @@ controller.getAll = async function (req,res){
     }
 }
 
+controller.post = async function (req,res){
+    try {
+        const response = await axios({
+            method: 'POST',
+            url : 'http://jsonplaceholder.typicode.com/posts',
+            data: {
+                title: req.body.title,
+                body: req.body.body,
+                userId: 1
+            }
+        });
+       res.status(201).json({
+           message: 'Berhasil Tambah Data',
+           data: response.data
+       })
+    } catch (error) {
+        res.status(404).json({
+            message: error
+        })
+    }
+}
+
+controller.put = async function (req,res){
+    try {
+        var id = req.params.id
+        const response = await axios({
+            method: 'put',
+            url : 'http://jsonplaceholder.typicode.com/posts/'+id,
+            data: {
+                title: req.body.title,
+                body: req.body.body,
+                userId: 1
+            }
+        });
+       res.status(201).json({
+           message: 'Berhasil Edit Data',
+           data: response.data
+       })
+    } catch (error) {
+        res.status(404).json({
+            message: error
+        })
+    }
+}
+
+controller.delete = async function (req,res){
+    try {
+        var id = req.params.id
+        const response = await axios({
+            method: 'delete',
+            url : 'http://jsonplaceholder.typicode.com/posts/'+id
+        });
+       res.status(201).json({
+           message: 'Berhasil Hapus Data',
+           data: response.data
+       })
+    } catch (error) {
+        res.status(404).json({
+            message: error
+        })
+    }
+}
+
 module.exports = controller;
--- routes/axios.js
+++ routes/axios.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const controller = require('../controller/index');
 
 router.get('/', controller.axios.getAll);
+router.post('/', controller.axios.post);
+router.put('/:id', controller.axios.put);
+router.delete('/:id', controller.axios.delete);
 
 module.exports = router;
